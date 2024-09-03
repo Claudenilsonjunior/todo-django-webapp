9d7a160ff20b37f41a1b5b905c77ff5f1527e096
Claudenilson Júnior <claudenilsonjunior2@gmail.com>
2024-09-03 05:13:34 -0300

    Initial Backend Complete

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, logout_view, RegisterView
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task/create/', TaskCreate.as_view(), name='task_create'),
+    path('task/<int:pk>/update/', TaskUpdate.as_view(), name='task_update'),
+    path('task/<int:pk>/delete/', DeleteView.as_view(), name='task_delete'),
+]

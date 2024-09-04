c9045596c7d51394c87aac8efba3c346b80b5e5e
Claudenilson Júnior <claudenilsonjunior2@gmail.com>
2024-09-04 20:36:20 -0300

    Update settings.py

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -120,6 +120,10 @@ LOGIN_URL = 'login' # Redirect to login page
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "base/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
